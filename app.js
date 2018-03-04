5825918fa93dd0418fcd69445c230b29c2b91896
Yuhaonan <272775425@qq.com>
2018-03-04 10:16:29 +0800

    入门
    vue配置 事例对象 常用函数注解

---
+++ app.js
@@ -0,0 +1,22 @@
+//事例化vue对象
+new Vue({
+	el:"#vue-app",
+	data:{
+		// name:"浩男",
+		// job:"job",
+		// website:"http://haofanmedia.com",
+		// websiteTag:"<a href='http://haofanmeida.com'>标签</a>"
+		   age:"25"
+	},
+	// methods:{
+	// 	greet:function(time){
+	// 		return 'good '+time+'!';
+	// 	}
+	// }
+});
+
+
+// el:代表元素element 需要获取的元素一定是html中的根容器元素
+//data 用以数据的存贮 设置各种数据 data是一个对象 他的key和value的值是自己定义的
+// methods 用于存储各种方法
+//data-binding:给属性绑定对应的值
